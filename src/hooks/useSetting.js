4d3d9fa34baec67c57dcab4827e84c6af0c582ae
Yemelyanov <nikolaef.post@gmail.com>
2024-01-23 14:12:00 +0200

    feat: add language context+

--- src/hooks/useSetting.js
+++ src/hooks/useSetting.js
@@ -4,7 +4,15 @@ import {useLanguage} from "../context/index.js";
 const useSetting = () => {
     const { text } = useLanguage();
 
-    const [settingsData, setSettingsData] = useState({})
+    const [settingsData, setSettingsData] = useState({
+        uvi: {value: true, label: text.uv_index},
+        sunrise: {value: true, label: text.sunrise},
+        wind: {value: true, label: text.wind},
+        feelsLike: {value: true, label: text.feels_like},
+        humidity: {value: true, label: text.humidity},
+        visibility: {value: true, label: text.visibility},
+        pressure: {value: true, label: text.pressure},
+    })
 
     // useEffect(() => {
     //     const settingsDataStorage = JSON.parse(localStorage.getItem("settings"));
