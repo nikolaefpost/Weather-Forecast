20f4bc69f7ad94b903b56b66792ee5d63df5ad91
Yemelyanov <nikolaef.post@gmail.com>
2024-01-23 14:48:00 +0200

    feat: add language context+

--- src/hooks/useSetting.js
+++ src/hooks/useSetting.js
@@ -22,16 +22,29 @@ const useSetting = () => {
     useEffect(() => {
         const settingsDataStorage = JSON.parse(localStorage.getItem("settings"));
         if (settingsDataStorage) setSettingsData(settingsDataStorage);
-        else setSettingsData({
-            uvi: {value: true, label: text.uv_index},
-            sunrise: {value: true, label: text.sunrise},
-            wind: {value: true, label: text.wind},
-            feelsLike: {value: true, label: text.feels_like},
-            humidity: {value: true, label: text.humidity},
-            visibility: {value: true, label: text.visibility},
-            pressure: {value: true, label: text.pressure},
-        })
-    }, [text])
+        // else setSettingsData({
+        //     uvi: {value: true, label: text.uv_index},
+        //     sunrise: {value: true, label: text.sunrise},
+        //     wind: {value: true, label: text.wind},
+        //     feelsLike: {value: true, label: text.feels_like},
+        //     humidity: {value: true, label: text.humidity},
+        //     visibility: {value: true, label: text.visibility},
+        //     pressure: {value: true, label: text.pressure},
+        // })
+    }, [])
+
+    useEffect(() => {
+        setSettingsData((prevSettingsData) => ({
+            ...prevSettingsData,
+            uvi: { ...prevSettingsData.uvi, label: text.uv_index },
+            sunrise: { ...prevSettingsData.sunrise, label: text.sunrise },
+            wind: { ...prevSettingsData.wind, label: text.wind },
+            feelsLike: { ...prevSettingsData.feelsLike, label: text.feels_like },
+            humidity: { ...prevSettingsData.humidity, label: text.humidity },
+            visibility: { ...prevSettingsData.visibility, label: text.visibility },
+            pressure: { ...prevSettingsData.pressure, label: text.pressure },
+        }));
+    }, [text]);
 
     return {settingsData, setSettingsData}
 }
